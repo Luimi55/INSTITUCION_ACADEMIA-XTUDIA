3289884d14bf68009d06d189488d1d64a626449b
Luis Miguel Rosa <Miguelrosa5@hotmail.es>
2022-04-20 11:25:41 -0400

    [feat]: Created Estudiante create view

---
+++ INSTITUCION_ACADEMIA XTUDIA/Controllers/EstudianteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INSTITUCION_ACADEMIA_XTUDIA.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: Estudiante
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
