ef233fe86ad16ef9ee8fc7205c9938b94220d302
Luis Miguel Rosa <Miguelrosa5@hotmail.es>
2022-04-20 10:19:58 -0400

    [feat]: Created Correo Model

---
+++ INSTITUCION_ACADEMIA XTUDIA/Models/Correo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INSTITUCION_ACADEMIA_XTUDIA.Models
+{
+    public class Correo
+    {
+        public int correoid { get; set; }
+        public string correoelectronico { get; set; }
+        public System.Guid tutorid { get; set; }
+        public Nullable<System.DateTime> fecha_creacion { get; set; }
+        public int Tutor { get; set; }
+    }
+}
