acac4f09ce7b1272fbc1a21517290bd1a72bc1ea
Luis Miguel Rosa <Miguelrosa5@hotmail.es>
2022-04-20 10:16:50 -0400

    [feat]: Created Tutores Model

---
+++ INSTITUCION_ACADEMIA XTUDIA/Models/Tutores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INSTITUCION_ACADEMIA_XTUDIA.Models
+{
+    public class Tutores
+    {
+        public System.Guid tutorid { get; set; }
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public System.DateTime fechanacimiento { get; set; }
+        public int lugarnacimientoid { get; set; }
+        public string fotografia { get; set; }
+        public int estadocivilid { get; set; }
+        public string lugartrabajo { get; set; }
+        public string direccion { get; set; }
+        public Nullable<System.DateTime> fecha_creacion { get; set; }
+        public int tipotutorid { get; set; }
+    }
+}
